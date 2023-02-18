6542cedfc9f4c21d6ce029e1a99f5d096265bc43
anuffisenough <uffmanch@gmail.com>
2023-02-17 19:25:55 -0700

    adds array of question prompts

--- Nodejs_Challenge/Develop/index.js
+++ Nodejs_Challenge/Develop/index.js
@@ -1,13 +1,55 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?", 
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "Please enter a description of your application here:",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "Please provide installation instructions for your application:",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "Please provide information about your application's usage here:",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "Please provide guidelines for contributions to your project here:",
+        name: "contributing",
+    },
+    {
+        type: "input",
+        message: "Please provide test instructions for your application here:",
+        name: "tests"
+    },
+    {
+        type: "checkbox",
+        message: "Please choose a license for your application from the following options:",
+        name: "license",
+        choices: ["gnu", "mit", "mozilla"],
+    }
+     ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer
+.prompt(questions);
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// Function call to initialize app
-init();
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
